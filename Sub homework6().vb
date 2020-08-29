5f3ab6ce64a4ee54f0d08622376d0ce677e5e727
Jeremy Steele <jeremysteele@Jeremys-MacBook-Pro.local>
2020-08-29 12:47:26 -0700

    Vba homework

---
+++ Sub homework6().vb
@@ -0,0 +1,55 @@
+Sub homework6()
+
+Dim Ticker As String
+Dim Change As Integer
+Dim Percent As Single
+Dim Total_Stock As Double
+Dim Closing As Single
+Dim Opening As Single
+Dim Sum As Integer
+Dim Lastrow As Long
+
+Total_Stock = 0
+Sum = 2
+
+For Each Sheet In Worksheets
+
+Lastrow = Sheet.Cells(Rows.Count, 1).End(xlUp).Row
+
+For j = 2 To Lastrow
+
+    If Sheet.Cells(j - 1, 1).Value <> Sheet.Cells(j, 1) Then
+        Opening = Sheet.Cells(j, 3)
+    End If
+
+    If Sheet.Cells(j + 1, 1).Value <> Sheet.Cells(j, 1).Value Then
+        Ticker = Sheet.Cells(j, 1)
+        Sheet.Cells(Sum, 9) = Ticker
+        Closing = Sheet.Cells(j, 6).Value
+        Sheet.Cells(Sum, 10).Value = Closing - Opening
+        Sheet.Cells(Sum, 11).Value = Sheet.Cells(Sum, 10).Value / Opening
+    
+    If Sheet.Cells(Sum, 10).Value > 0 Then
+        Sheet.Cells(Sum, 10).Interior.ColorIndex = 4
+    End If
+        
+     If Sheet.Cells(Sum, 10).Value < 0 Then
+        Sheet.Cells(Sum, 10).Interior.ColorIndex = 3
+    End If
+    
+        Total_Stock = Total_Stock + Sheet.Cells(j, 7).Value
+        Sheet.Cells(Sum, 12) = Total_Stock
+        Total_Stock = 0
+        Sum = Sum + 1
+     
+    Else: Sheet.Cells(j + 1, 1).Value = Sheet.Cells(j, 1).Value
+        Total_Stock = Total_Stock + Sheet.Cells(j, 7).Value
+    End If
+    
+    Sheet.Cells(Sum, 10).NumberFormat = "0.00#"
+    Sheet.Cells(Sum, 11).NumberFormat = "0.00%"
+Next j
+
+    Sum = 2
+Next Sheet
+End Sub
